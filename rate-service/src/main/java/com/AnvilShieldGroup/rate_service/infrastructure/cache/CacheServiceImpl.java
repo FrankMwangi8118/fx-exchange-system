824032239cfd639e5bcd60f5a54919dd0afd16af
CODIFY <frankmwangi8118@gmail.com>
2025-05-09 12:54:22 +0300

    feat: added caffeine cache impl

---
+++ rate-service/src/main/java/com/AnvilShieldGroup/rate_service/infrastructure/cache/CacheServiceImpl.java
@@ -0,0 +1,61 @@
+package com.AnvilShieldGroup.rate_service.infrastructure.cache;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@Slf4j
+public class CacheServiceImpl {
+    // cache structure: key: base currency (eg"USD"),value Map(target/to currency,rate)
+    //"USD",<"CAD":1.45333>
+
+    private final Cache<String, Map<String, Double>> cache;
+
+    public CacheServiceImpl(Cache<String, Map<String, Double>> cache) {
+        this.cache = Caffeine.newBuilder()
+                .expireAfterAccess(10, TimeUnit.MINUTES)
+                .maximumSize(2000)
+                .build();
+    }
+
+    /**
+     * Attempts to retrieve a cached exchange rate from the 'from' to 'to' currency.
+     *
+     * @param from base currency (e.g., "USD")
+     * @param to   target currency (e.g., "CAD")
+     * @return the exchange rate if present, otherwise null
+     */
+
+    public Double getRateFromCache(String from, String to) {
+        Map<String, Double> rates = cache.getIfPresent(from);
+        if (!rates.isEmpty()) {
+            rates.get(to);
+        log.info("cache hit for:{}:{}",to,rates.get(to));
+        }
+        return null;
+    }
+    /**
+     * Puts new rates in the cache for a given base currency.
+     * If there's already a map for that base currency, it merges the values.
+     *
+     * @param from     base currency (e.g., "USD")
+     * @param newRates map of target currency → rate (e.g., { "CAD": 143.0, "EUR": 0.91 })
+     */
+
+    public void putRate(String from, HashMap<String, Double> newRates) {
+        Map<String,Double>existingRates=cache.getIfPresent(from);
+        if(existingRates==null){
+            cache.put(from,newRates); // safe to insert since data is non-existing
+        }else{
+            existingRates.putAll(newRates);
+            cache.put(from,existingRates); //re-put the merged map
+        }
+    }
+
+}
--- rate-service/src/main/java/com/AnvilShieldGroup/rate_service/service/ExchangeRateServiceImpl.java
+++ rate-service/src/main/java/com/AnvilShieldGroup/rate_service/service/ExchangeRateServiceImpl.java
@@ -2,34 +2,61 @@ package com.AnvilShieldGroup.rate_service.service;
 
 import com.AnvilShieldGroup.rate_service.controller.Dto.RequestDto;
 import com.AnvilShieldGroup.rate_service.controller.Dto.ResponseDto;
+import com.AnvilShieldGroup.rate_service.infrastructure.cache.CacheServiceImpl;
 import com.AnvilShieldGroup.rate_service.infrastructure.external.ExchangeRateClient;
 import com.AnvilShieldGroup.rate_service.infrastructure.external.dto.ExternalExchangeRateResponseDto;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class ExchangeRateServiceImpl implements ExchangeRateService {
     private final ExchangeRateClient exchangeRateClient;
+    private final CacheServiceImpl cacheService;
 
-    public ExchangeRateServiceImpl(ExchangeRateClient exchangeRateClient) {
+    public ExchangeRateServiceImpl(ExchangeRateClient exchangeRateClient, CacheServiceImpl cacheService) {
         this.exchangeRateClient = exchangeRateClient;
+        this.cacheService = cacheService;
     }
 
     @Override
     public Mono<ResponseDto> getCurrencyQuote(RequestDto requestDto) {
+
         return getCurrencyQuoteFromExternal(requestDto);
     }
 
     @Override
     public Mono<ResponseDto> getCurrencyQuoteFromExternal(RequestDto requestDto) {
-        return exchangeRateClient.fetchExchangeRate(requestDto.getFrom(), requestDto.getTo())
-                .map(res -> ResponseDto.builder()
-                        .from(requestDto.getFrom())
-                        .to(requestDto.getTo())
-                        .rate(res.getData().get(requestDto.getTo()))
-                        .build());
+        String from = requestDto.getFrom();
+        String to = requestDto.getTo();
+
+        Double cachedRate = cacheService.getRateFromCache(from, to);
+        if (cachedRate != null){
+            return Mono.just(ResponseDto.builder()
+                            .from(from)
+                            .to(to)
+                            .rate(cachedRate)
+                    .build());
+        }
+        return exchangeRateClient.fetchExchangeRate(from,to)
+                .map(res->{
+                Double rate=res.getData().get(to);
+                    passToCacheService(from,to,rate);
+                    return ResponseDto.builder()
+                            .from(from)
+                            .to(to)
+                            .rate(rate)
+                            .build();
+                });
+    }
+    private void passToCacheService(String from ,String to,Double rate){
+        HashMap<String, Double>rateMap=new HashMap<>();
+        rateMap.put(to,rate);
+        cacheService.putRate(from,rateMap);
+
     }
 
 
-
-}
+    }
