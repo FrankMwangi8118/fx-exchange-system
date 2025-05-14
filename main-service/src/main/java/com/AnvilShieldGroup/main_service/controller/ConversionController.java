3a98362731c1a1d7a830d65c7ba18c63a6535781
CODIFY <frankmwangi8118@gmail.com>
2025-05-14 10:56:25 +0300

    feat: added swagger api documentation to  mainService

--- main-service/src/main/java/com/AnvilShieldGroup/main_service/controller/ConversionController.java
+++ main-service/src/main/java/com/AnvilShieldGroup/main_service/controller/ConversionController.java
@@ -3,21 +3,28 @@ package com.AnvilShieldGroup.main_service.controller;
 import com.AnvilShieldGroup.main_service.controller.dto.ApiResponse;
 import com.AnvilShieldGroup.main_service.controller.dto.RequestDto;
 import com.AnvilShieldGroup.main_service.service.ConversionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/mainService")
 @AllArgsConstructor
+@Tag(name = "Currency Conversion API", description = "Converts currency values")
 public class ConversionController {
+
     private final ConversionService conversionService;
 
-    @GetMapping
-    public Mono<ResponseEntity<ApiResponse>> convert(@RequestBody RequestDto requestDto) {
+    @Operation(summary = "Convert currency", description = "Converts amount from one currency to another")
+    @PostMapping("/convert")
+    public Mono<ResponseEntity<ApiResponse>> convert(@RequestBody @Valid RequestDto requestDto) {
         return conversionService.convertCurrency(requestDto)
                 .map(responseDto -> ResponseEntity.ok(
                         ApiResponse.builder()
@@ -27,4 +34,16 @@ public class ConversionController {
                                 .data(responseDto)
                                 .build()
                 ));
-    }}
+    }
+
+    @GetMapping("/status")
+    @Operation(
+            summary = "gets the status of the service",
+            description = "checks the availability of the server"
+    )
+    public ResponseEntity<?> status() {
+        Map<String, String> status = new HashMap<>();
+        status.put("status", "up");
+        return ResponseEntity.ok().body(status);
+    }
+}
